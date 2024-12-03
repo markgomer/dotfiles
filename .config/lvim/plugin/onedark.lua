704044617d12ae4d47938f059347b98fb0c6c30b
markgomer <aureliojuniorcmrj@hotmail.com>
2024-12-03 11:29:56 -0300

    Fix links from lunarvim plugin config files

---
+++ .config/lvim/plugin/onedark.lua
@@ -0,0 +1,20 @@
+local options = {
+    term_colors = true, -- Change terminal color as per the selected theme style
+    ending_tildes = true, -- Show the end-of-buffer tildes. By default they are hidden
+    style = 'deep', -- 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer' and 'light'
+    -- Change code style ---
+    -- Options are italic, bold, underline, none
+    -- You can configure multiple style with comma separated, For e.g., keywords = 'italic,bold'
+    code_style = {
+        comments = 'italic',
+        keywords = 'none',
+        functions = 'none',
+        strings = 'none',
+        variables = 'none'
+    },
+    highlights = {
+        ["@lsp.type.variable"] = {fg = '$red'},
+        ["@variable"] = {fg = '$red'},
+    }
+}
+require('onedark').setup(options)
