82103cdd89d0504b244595ce083afde41c9aba78
markgomer <aureliojuniorcmrj@hotmail.com>
2025-06-08 09:38:07 -0300

    Tweak energy profiles

--- .local/scripts/equilibrado.sh
+++ .local/scripts/equilibrado.sh
@@ -4,7 +4,8 @@ echo 1 > /sys/devices/system/cpu/cpu4/online; \
 echo 1 > /sys/devices/system/cpu/cpu5/online; \
 echo 1 > /sys/devices/system/cpu/cpu6/online; \
 echo 1 > /sys/devices/system/cpu/cpu7/online; \
-echo "1100" > /sys/class/drm/card1/gt_max_freq_mhz; \
+echo "600" > /sys/class/drm/card1/gt_min_freq_mhz; \
+echo "900" > /sys/class/drm/card1/gt_max_freq_mhz; \
 echo "1100" > /sys/class/drm/card1/gt_boost_freq_mhz'
 sudo cpupower set --perf-bias 15
 sudo cpupower frequency-set -g powersave -d 800MHz -u 2.4GHz
