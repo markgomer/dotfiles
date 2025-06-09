e8d7acd667b56bfc32c6fa1443417cc7f54ab1a7
markgomer <aureliojuniorcmrj@hotmail.com>
2025-06-08 22:14:11 -0300

    Remove nix config for zsh

--- .config/nixos-config/hosts/avell/user/majunior.nix
+++ .config/nixos-config/hosts/avell/user/majunior.nix
@@ -68,20 +68,6 @@ in
       promptInit = ''
         #pokemon colorscripts like. Make sure to install krabby package
         #krabby random --no-mega --no-gmax --no-regional --no-title -s; 
-        # Set-up icons for files/directories in terminal using lsd
-        alias ls='lsd'
-        alias l='ls -l'
-        alias la='ls -a'
-        alias lla='ls -la'
-        alias lt='ls --tree'
-
-        # source <(fzf --zsh);
-        # source ${pkgs.zsh-powerlevel10k}/share/zsh-powerlevel10k/powerlevel10k.zsh-theme
-
-        # HISTFILE=~/.zsh_history;
-        # HISTSIZE=10000;
-        # SAVEHIST=10000;
-        # setopt appendhistory;
       '';
 
       shellAliases = {
