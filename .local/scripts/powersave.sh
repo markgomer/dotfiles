0ab87311ea4c90a3655c7f5bcbf4d7445c8ac08d
markgomer <aureliojuniorcmrj@hotmail.com>
2025-06-11 16:15:45 -0300

    [Scripts] Update energy profile scripts

--- .local/scripts/powersave.sh
+++ .local/scripts/powersave.sh
@@ -1,11 +1,17 @@
 #!/usr/bin/env bash
+sudo sh -c 'echo 1 > /sys/devices/system/cpu/cpu4/online; \
+echo 1 > /sys/devices/system/cpu/cpu5/online; \
+echo 1 > /sys/devices/system/cpu/cpu6/online; \
+echo 1 > /sys/devices/system/cpu/cpu7/online;' 
+
+sudo cpupower set --perf-bias 15
+sudo cpupower frequency-set -g powersave -d 800MHz -u 1100MHz
+
 sudo sh -c 'echo 1 > /sys/devices/system/cpu/intel_pstate/no_turbo; \
 echo 0 > /sys/devices/system/cpu/cpu4/online; \
 echo 0 > /sys/devices/system/cpu/cpu5/online; \
 echo 0 > /sys/devices/system/cpu/cpu6/online; \
 echo 0 > /sys/devices/system/cpu/cpu7/online; \
-echo "600" > /sys/class/drm/card1/gt_min_freq_mhz; \
-echo "650" > /sys/class/drm/card1/gt_max_freq_mhz; \
-echo "700" > /sys/class/drm/card1/gt_boost_freq_mhz'
-sudo cpupower set --perf-bias 15
-sudo cpupower frequency-set -g powersave -d 800MHz -u 1300MHz
+echo "500" > /sys/class/drm/card1/gt_min_freq_mhz; \
+echo "700" > /sys/class/drm/card1/gt_max_freq_mhz; \
+echo "700" > /sys/class/drm/card1/gt_boost_freq_mhz;'
