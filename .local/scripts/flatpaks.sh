00a83aec09b4552fc445b3d8e1246a8442aa6cc3
markgomer <aureliojuniorcmrj@hotmail.com>
2025-07-08 13:37:02 -0300

    Update flatpak list

--- .local/scripts/flatpaks.sh
+++ .local/scripts/flatpaks.sh
@@ -1,12 +1,11 @@
-flatpak install --user flathub app.zen_browser.zen \
-  com.github.tchx84.Flatseal \
-  org.gnome.baobab \
-  io.github.flattool.Warehouse \
-  net.lutris.Lutris \
-  com.vysp3r.ProtonPlus \
-  page.kramo.Cartridges \
-  dev.vencord.Vesktop \
-  com.github.marhkb.Pods \
-  com.ranfdev.DistroShelf \
-  md.obsidian.Obsidian \
-  org.qbittorrent.qBittorrent
+flatpak install app.zen_browser.zen \
+    com.github.tchx84.Flatseal \
+    org.gnome.baobab \
+    io.github.flattool.Warehouse \
+    com.vysp3r.ProtonPlus \
+    dev.vencord.Vesktop \
+    com.github.marhkb.Pods \
+    com.ranfdev.DistroShelf \
+    md.obsidian.Obsidian \
+    org.qbittorrent.qBittorrent \
+    com.github.zocker_160.SyncThingy
