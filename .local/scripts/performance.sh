15a683e37d8980fec3673551b2414bbde979df2b
markgomer <aureliojuniorcmrj@hotmail.com>
2025-07-08 13:36:35 -0300

    [scripts] update videocard indexes

--- .local/scripts/performance.sh
+++ .local/scripts/performance.sh
@@ -4,9 +4,9 @@ echo 1 > /sys/devices/system/cpu/cpu4/online; \
 echo 1 > /sys/devices/system/cpu/cpu5/online; \
 echo 1 > /sys/devices/system/cpu/cpu6/online; \
 echo 1 > /sys/devices/system/cpu/cpu7/online; \
-echo "600" > /sys/class/drm/card1/gt_min_freq_mhz; \
-echo "1100" > /sys/class/drm/card1/gt_max_freq_mhz; \
-echo "1100" > /sys/class/drm/card1/gt_boost_freq_mhz;'
+echo "500" > /sys/class/drm/card0/gt_min_freq_mhz; \
+echo "1100" > /sys/class/drm/card0/gt_max_freq_mhz; \
+echo "1100" > /sys/class/drm/card0/gt_boost_freq_mhz;'
 
 sudo cpupower set --perf-bias 15
 sudo cpupower frequency-set -g performance -d 800MHz -u 3.8GHz
