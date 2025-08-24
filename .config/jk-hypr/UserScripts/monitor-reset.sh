d6d6a0c9309e52d186452f09197813569be40915
Marco Aurélio S.S.Jr. <aureliojuniorcmrj@hotmail.com>
2025-08-24 15:38:27 -0300

    move jk-hypr to hypr.
    * That's my main config now

--- .config/jk-hypr/UserScripts/monitor-reset.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Monitor power events and switch to laptop screen
-hyprctl keyword monitor "eDP-1,preferred,0x0,1"
-hyprctl keyword monitor "HDMI-A-1,disable"
-sleep 1
-loginctl lock-session
