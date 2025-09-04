0425e98273827493b28ebe0e225d3b1e5a25f730
Marco Aurélio S.S.Jr. <aureliojuniorcmrj@hotmail.com>
2025-09-04 20:19:00 -0300

    [scripts] update power scripts iGPU location

--- .local/bin/equilibrado.sh
+++ .local/bin/equilibrado.sh
@@ -4,9 +4,9 @@ echo 1 > /sys/devices/system/cpu/cpu4/online; \
 echo 1 > /sys/devices/system/cpu/cpu5/online; \
 echo 1 > /sys/devices/system/cpu/cpu6/online; \
 echo 1 > /sys/devices/system/cpu/cpu7/online; \
-echo "350" > /sys/class/drm/card0/gt_min_freq_mhz; \
-echo "700" > /sys/class/drm/card0/gt_max_freq_mhz; \
-echo "1100" > /sys/class/drm/card0/gt_boost_freq_mhz;'
+echo "350" > /sys/class/drm/card2/gt_min_freq_mhz; \
+echo "700" > /sys/class/drm/card2/gt_max_freq_mhz; \
+echo "1100" > /sys/class/drm/card2/gt_boost_freq_mhz;'
 
 sudo cpupower set --perf-bias 15
 sudo cpupower frequency-set -g powersave -d 800MHz -u 2.4GHz
