61ad379be809324ed584320eeb002f607198930e
Marco Aurélio S.S.Jr. <aureliojuniorcmrj@hotmail.com>
2025-09-06 10:41:45 -0300

    [zsh] move pokefetch to a function

--- .config/zsh/init.sh
+++ .config/zsh/init.sh
@@ -1,28 +1,18 @@
-if command -v mise &> /dev/null; then
-  eval "$(mise activate bash)"
+if command -v mise &>/dev/null; then
+    eval "$(mise activate bash)"
 fi
 
-if command -v zoxide &> /dev/null; then
-  eval "$(zoxide init bash)"
+if command -v zoxide &>/dev/null; then
+    eval "$(zoxide init bash)"
 fi
 
-if command -v fzf &> /dev/null; then
-  if [[ -f /usr/share/bash-completion/completions/fzf ]]; then
-    source /usr/share/bash-completion/completions/fzf
-  fi
-  if [[ -f /usr/share/doc/fzf/examples/key-bindings.bash ]]; then
-    source /usr/share/doc/fzf/examples/key-bindings.bash
-  fi
+if command -v fzf &>/dev/null; then
+    if [[ -f /usr/share/bash-completion/completions/fzf ]]; then
+        source /usr/share/bash-completion/completions/fzf
+    fi
+    if [[ -f /usr/share/doc/fzf/examples/key-bindings.bash ]]; then
+        source /usr/share/doc/fzf/examples/key-bindings.bash
+    fi
 fi
 
-# get the zellij session name
-export ZELLIJ_POKEMON_NAME=$(zellij list-sessions | \grep '(current)' | sed -r "s/\x1b\[[0-9;]*m//g" | awk '{print $1}')
-
-if command -v fastfetch &> /dev/null; then
-  if [ -n "$ZELLIJ_POKEMON_NAME" ]; then
-    pokemon-colorscripts --no-title -n "$ZELLIJ_POKEMON_NAME" | fastfetch --logo-type file-raw --logo-height 10 --logo-width 5 --logo -
-  else
-    # show a random pokemon instead
-    pokemon-colorscripts --no-title -r | fastfetch --logo-type file-raw --logo-height 10 --logo-width 5 --logo -
-  fi
-fi
+pokefetch
