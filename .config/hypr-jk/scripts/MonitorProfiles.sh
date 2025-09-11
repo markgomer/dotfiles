efd229df2cf5ee5704d2c3ef86bc7559c81512f6
Marco Aurélio S.S.Jr. <aureliojuniorcmrj@hotmail.com>
2025-09-11 20:59:52 -0300

    [hypr] rename monitor profiles folder to pattern

--- .config/hypr-jk/scripts/MonitorProfiles.sh
+++ .config/hypr-jk/scripts/MonitorProfiles.sh
@@ -10,7 +10,7 @@ fi
 # Variables
 iDIR="$HOME/.config/swaync/images"
 SCRIPTSDIR="$HOME/.config/hypr/scripts"
-monitor_dir="$HOME/.config/hypr/Monitor_Profiles"
+monitor_dir="$HOME/.config/hypr/monitor_profiles"
 target="$HOME/.config/hypr/monitors.conf"
 rofi_theme="$HOME/.config/rofi/config-Monitors.rasi"
 msg='❗NOTE:❗ This will overwrite $HOME/.config/hypr/monitors.conf'
