b272eae114731b8f9b8a67dff8a9003bf9b3fbec
Marco Aurélio S.S.Jr. <aureliojuniorcmrj@hotmail.com>
2025-09-16 20:45:23 -0300

    [zsh] remove pokemon aliases. It is now a function

--- .config/zsh/aliases.sh
+++ .config/zsh/aliases.sh
@@ -1,10 +1,6 @@
 alias lzg='lazygit'
 alias lzd='lazydocker'
 
-alias poke='pokemon-colorscripts -n $ZELLIJ_POKEMON_NAME'
-alias pokefetch='pokemon-colorscripts --no-title -n "$ZELLIJ_POKEMON_NAME" | \
-  fastfetch --logo-type file-raw --logo-height 10 --logo-width 5 --logo -'
-
 alias edf='nvim ~/dotfiles'
 alias ujust='just -g'
 
