e3895eb4cea2b71bfc21dcaa1df0cf45d917aff2
Marco Aurélio S.S.Jr. <aureliojuniorcmrj@hotmail.com>
2025-10-20 12:07:52 -0300

    zsh: zellij launchers binds

--- .config/zsh/aliases.sh
+++ .config/zsh/aliases.sh
@@ -8,3 +8,6 @@ alias ujust='just -g'
 alias sv="$HOME/dotfiles/.local/bin/powersave.sh"
 alias eq="$HOME/dotfiles/.local/bin/equilibrado.sh"
 alias pw="$HOME/dotfiles/.local/bin/performance.sh"
+
+bindkey -s ^f "zellij-picker $HOME/Projects/ \n"
+bindkey -s ^n "pokezellij \n"
