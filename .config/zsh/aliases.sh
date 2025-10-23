4592f30c740f20ed846a64f9e7f083dc6f64ff29
Marco Aurelio S.S.Jr. <aureliojuniorcmrj@hotmail.com>
2025-10-23 09:56:38 -0300

    zsh: remove power management aliases

--- .config/zsh/aliases.sh
+++ .config/zsh/aliases.sh
@@ -5,9 +5,5 @@ alias edf='nvim ~/dotfiles' # Edit dotfiles
 alias eom='nvim ~/.local/share/omarchy' # Edit Omarchy
 alias ujust='just -g'
 
-alias sv="$HOME/dotfiles/.local/bin/powersave.sh"
-alias eq="$HOME/dotfiles/.local/bin/equilibrado.sh"
-alias pw="$HOME/dotfiles/.local/bin/performance.sh"
-
 bindkey -s ^f "zellij-picker $HOME/Projects/ \n"
 bindkey -s ^n "pokezellij \n"
