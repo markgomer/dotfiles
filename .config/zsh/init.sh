6a308a3b94a0afccc33b4172919a659c1366f2fa
Marco Aurelio S.S.Jr. <aureliojuniorcmrj@hotmail.com>
2025-10-23 10:15:03 -0300

    zsh: remove unused lines

--- .config/zsh/init.sh
+++ .config/zsh/init.sh
@@ -6,13 +6,6 @@ if command -v starship &> /dev/null; then
   eval "$(starship init zsh)"
 fi
 
-if command -v fzf &>/dev/null; then
-    if [[ -f /usr/share/bash-completion/completions/fzf ]]; then
-        source /usr/share/bash-completion/completions/fzf
-    fi
-    if [[ -f /usr/share/doc/fzf/examples/key-bindings.bash ]]; then
-        source /usr/share/doc/fzf/examples/key-bindings.bash
-    fi
 if command -v zoxide &>/dev/null; then
     eval "$(zoxide init zsh)"
 fi
