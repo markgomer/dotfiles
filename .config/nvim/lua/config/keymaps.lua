a3ee0d914123bbf7c64c80c4a13a97b6d51e212d
Marco Aurélio S.S.Jr. <aureliojuniorcmrj@hotmail.com>
2025-11-02 11:59:56 -0300

    nvim: jump to previous tmux session is enough for me

--- .config/nvim/lua/config/keymaps.lua
+++ .config/nvim/lua/config/keymaps.lua
@@ -25,10 +25,7 @@ function SetupKeymaps()
 
     -- tmux-sessionizer
     vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
-    vim.keymap.set("n", "<M-h>", "<cmd>silent !tmux neww tmux-sessionizer -s 0<CR>")
-    vim.keymap.set("n", "<M-j>", "<cmd>silent !tmux neww tmux-sessionizer -s 1<CR>")
-    vim.keymap.set("n", "<M-k>", "<cmd>silent !tmux neww tmux-sessionizer -s 2<CR>")
-    vim.keymap.set("n", "<M-l>", "<cmd>silent !tmux neww tmux-sessionizer -s 3<CR>")
+    vim.keymap.set("n", "<M-p>", "<cmd>silent !tmux switch-client -l<CR>")
 
     -- Replace token you're in
     vim.keymap.set(
