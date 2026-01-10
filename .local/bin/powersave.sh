a72c3383aadc90f936bc02ff01a9cfb802460c95
Marco Aurélio S.S.Jr. <aureliojuniorcmrj@hotmail.com>
2026-01-10 17:52:37 -0300

    remove core disabling in power scripts

--- .local/bin/powersave.sh
+++ .local/bin/powersave.sh
@@ -1,20 +1,15 @@
 #!/usr/bin/env bash
 
-sudo sh -c 'echo 1 > /sys/devices/system/cpu/cpu4/online; \
-echo 1 > /sys/devices/system/cpu/cpu5/online; \
-echo 1 > /sys/devices/system/cpu/cpu6/online; \
-echo 1 > /sys/devices/system/cpu/cpu7/online;'
+# sudo sh -c 'echo 1 > /sys/devices/system/cpu/cpu4/online; \
+# echo 1 > /sys/devices/system/cpu/cpu5/online; \
+# echo 1 > /sys/devices/system/cpu/cpu6/online; \
+# echo 1 > /sys/devices/system/cpu/cpu7/online;'
 
 sudo cpupower set --perf-bias 15
 sudo cpupower frequency-set -g powersave -d 800MHz -u 1100MHz
 
-sudo sh -c 'echo 1 > /sys/devices/system/cpu/intel_pstate/no_turbo; \
-echo 1 > /sys/devices/system/cpu/cpu4/online; \
-echo 1 > /sys/devices/system/cpu/cpu5/online; \
-echo 1 > /sys/devices/system/cpu/cpu6/online; \
-echo 1 > /sys/devices/system/cpu/cpu7/online; \
-echo "350" > /sys/class/drm/card1/gt_min_freq_mhz; \
-echo "450" > /sys/class/drm/card1/gt_max_freq_mhz; \
-echo "500" > /sys/class/drm/card1/gt_boost_freq_mhz;'
+sudo sh -c 'echo "350" > /sys/class/drm/card0/gt_min_freq_mhz; \
+echo "400" > /sys/class/drm/card0/gt_max_freq_mhz; \
+echo "450" > /sys/class/drm/card0/gt_boost_freq_mhz;'
 
 sudo intel-undervolt apply
