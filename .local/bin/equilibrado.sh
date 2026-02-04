4c60697a7e3272a3ff42c476d83de529ed12a9ab
Marco Aurélio S.S.Jr. <aureliojuniorcmrj@hotmail.com>
2026-02-03 21:56:40 -0300

    update lazyvim and power management script

--- .local/bin/equilibrado.sh
+++ .local/bin/equilibrado.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
 sudo sh -c 'echo "350" > /sys/class/drm/card0/gt_min_freq_mhz; \
-echo "600" > /sys/class/drm/card0/gt_max_freq_mhz; \
-echo "700" > /sys/class/drm/card0/gt_boost_freq_mhz;'
+echo "500" > /sys/class/drm/card0/gt_max_freq_mhz; \
+echo "600" > /sys/class/drm/card0/gt_boost_freq_mhz;'
 
 sudo cpupower set --perf-bias 15
-sudo cpupower frequency-set -g powersave -d 800MHz -u 1.6GHz
+sudo cpupower frequency-set -g powersave -d 800MHz -u 1.8GHz
 
 sudo intel-undervolt apply
