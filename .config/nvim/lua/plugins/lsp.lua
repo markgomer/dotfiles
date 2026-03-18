d9f420c5bd260f0164a27e5c67f6fba6c662dcec
Marco Aurélio S.S.Jr. <aureliojuniorcmrj@hotmail.com>
2026-03-18 16:27:08 -0300

    nvim: fix lspconfig and add nixd

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -1,12 +1,25 @@
 return {
-  {
-    "neovim/nvim-lspconfig",
-    opts = {
-      servers = {
-        clangd = {
-          mason = false,  -- Don't use Mason's clangd
-        },
-      },
+    {
+        "neovim/nvim-lspconfig",
+        opts = function(_, opts)
+            opts.servers = opts.servers or {}
+            opts.servers = {
+                clangd = {
+                    mason = false, -- Don't use Mason's clangd
+                },
+                nixd = {
+                    settings = {
+                        nixd = {
+                            nixpkgs = {
+                                expr = "import <nixpkgs> { }",
+                            },
+                            formatting = {
+                                command = { "alejandra" },
+                            },
+                        },
+                    },
+                },
+            }
+        end,
     },
-  },
 }
