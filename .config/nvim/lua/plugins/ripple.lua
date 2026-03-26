7c69ea3123452ab06baba53da6189898549b0d17
Marco Aurélio S.S.Jr. <aureliojuniorcmrj@hotmail.com>
2026-03-26 13:49:25 -0300

    nvim: default that ripple plugin to disabled

--- .config/nvim/lua/plugins/ripple.lua
+++ .config/nvim/lua/plugins/ripple.lua
@@ -1,14 +1,14 @@
 return {
     {
         "Cliffback/ripple-vscode-plugin.nvim",
-        config = function()
-            require("ripple-lsp").setup({
-                -- optional overrides
-                -- on_attach = function(client, bufnr) ... end
-                treesitter_lang = "tsx",
-                set_filetype = true,
-            })
-        end,
+        -- config = function()
+        --     require("ripple-lsp").setup({
+        --         -- optional overrides
+        --         -- on_attach = function(client, bufnr) ... end
+        --         treesitter_lang = "tsx",
+        --         set_filetype = true,
+        --     })
+        -- end,
     },
     {
         "Ripple-TS/ripple",
