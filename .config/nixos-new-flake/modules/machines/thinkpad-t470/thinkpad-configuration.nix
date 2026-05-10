23a484b90ea8da2bc964b376c51bfc5adfb5738b
Marco Aurélio S.S.Jr. <aureliojuniorcmrj@hotmail.com>
2026-05-10 18:37:58 -0300

    rename hypr configuration for avell machine specific

--- .config/nixos-new-flake/modules/machines/thinkpad-t470/thinkpad-configuration.nix
+++ .config/nixos-new-flake/modules/machines/thinkpad-t470/thinkpad-configuration.nix
@@ -148,6 +148,16 @@
             gvfs.enable = true;
 
             input-remapper.enable = true;
+
+            tlp = {
+                enable = true;
+                settings = {
+                    START_CHARGE_THRESH_BAT0 = 75;
+                    STOP_CHARGE_THRESH_BAT0 = 80;
+                    START_CHARGE_THRESH_BAT1 = 75;
+                    STOP_CHARGE_THRESH_BAT1 = 80;
+                };
+            };
         };
 
         security = {
